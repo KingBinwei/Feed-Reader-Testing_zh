01d5515c1058379692b9425dbfcbd22e36fcd33a
KingBinwei <k19990101@icloud.com>
2019-03-04 15:42:12 +0800

    测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的

--- src/jasmine/spec/feedreader.js
+++ src/jasmine/spec/feedreader.js
@@ -35,6 +35,12 @@ $(function() {
         /* TODO:
          * 编写一个测试遍历 allFeeds 对象里面的所有的源来保证有名字字段而且不是空的。
          */
+        it('names are present', function() {
+                for(var i = 0;i < allFeeds.length;i++) {
+                   expect(allFeeds[i].name).toBeDefined();
+                   expect(allFeeds[i].name).not.toBe(null);
+                }
+        });
     });
 
 
